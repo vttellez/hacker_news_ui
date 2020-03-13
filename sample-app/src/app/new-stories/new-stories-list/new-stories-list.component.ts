0613494b9286b97c5b07928ff459567253445584
Vladmir Varona <vvtellez@hotmail.com>
2020-03-12 22:53:53 -0400

    Adding more items

--- sample-app/src/app/new-stories/new-stories-list/new-stories-list.component.ts
+++ sample-app/src/app/new-stories/new-stories-list/new-stories-list.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NewStoryService } from '../service/new-stories.service';
-import { Observable } from 'rxjs';
+import { Observable, zip, of, forkJoin, merge } from 'rxjs';
 import { NewStory } from '../models/new-story.model';
+import { map, withLatestFrom } from 'rxjs/operators';
 
 @Component({
   selector: 'app-new-stories-list',
@@ -11,17 +12,53 @@ import { NewStory } from '../models/new-story.model';
 
 export class NewStoriesListComponent implements OnInit {
   stories$: Observable<NewStory[]>;
+  stories: NewStory[];
   title: 'Newes Stories';
   constructor(protected newStoryService: NewStoryService) { }
 
   ngOnInit(): void {
-    this.stories$ = this.newStoryService.loadStories();
+    this.newStoryService.loadStories().subscribe(x => {
+      if (x) {
+        this.stories = x;
+      }
+    }
+    );
 
     // .subscribe(x => {
     //   if (x) {
     //     console.log(x, 'result');
     //   }
     // });
+  }
+
+  seeMore() {
+    // zip(of([
+    //   {
+    //     url: '',
+    //     heading: '',
+    //     author: ''
+    //   }
+    // ]), this.stories$)
+    //   .pipe(map(x => x[0].concat(x[1])))
+    //   .subscribe(data => console.log('merge', data))\
+
+    const g: Observable<NewStory[]> = of([
+      {
+        id: '',
+        url: '',
+        heading: 'Vlad',
+        author: 'Vladimir'
+      }
+    ]);
+
+    this.stories = [...this.stories, ...[
+      {
+        id: '',
+        url: '',
+        heading: 'Vlad',
+        author: 'Vladimir'
+      }
+    ]];
   }
 
 }
